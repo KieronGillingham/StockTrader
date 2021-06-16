564b92c1ccd7d83a1e1127d21904969c2f876a1b
Kieron Gillingham <kierongillingham@gmail.com>
2021-06-16 09:53:10 +0100

    user sign-in logic and testing

--- testing/test_users.py
+++ testing/test_users.py
@@ -3,6 +3,8 @@ log_template = "[%(asctime)s] %(levelname)s %(threadName)s %(name)s: %(message)s
 logging.basicConfig(level=logging.DEBUG, format=log_template, handlers= [logging.FileHandler("debug.log"), logging.StreamHandler()])
 _logger = logging.getLogger(__name__)
 
+import pandas as pd
+
 from unittest import TestCase
 
 from users import UserManager
@@ -14,17 +16,32 @@ class TestUserManager(TestCase):
     def setUp(self):
         _logger.info(f"Setting up {__name__}")
         self.test_users_file = path.abspath("test_users.csv")
-        self.user_manager = UserManager(csv_path=self.test_users_file)
+
+        indices = ["bobjones","charliesmith@solfintech.co.uk","megacorp@megacorp.co.uk"]
+        columns = ["password", "balance"]
+        data = [
+            ["abc123", 100.00],
+            ["cat001", 250.00],
+            ["xyz321", 100000.00]
+        ]
+        test_users_data = pd.DataFrame(data=data, index=indices, columns=columns)
+        self.user_manager = UserManager(csv_path=None)
+        self.assertIsNone(self.user_manager.userbase, "Userbase created from no path.")
+        self.user_manager.userbase = test_users_data
         self.assertIsNotNone(self.user_manager, "UserManager constructor failed to return instance.")
+        self.assertIsNotNone(self.user_manager.userbase, "Userbase override failed.")
+        self.assertIsNone(self.user_manager.current_user, "Current user is not None.")
 
     def test_load_userbase(self):
         _logger.info("Testing load_userbase()")
+        self.user_manager = UserManager(csv_path=self.test_users_file)
         self.assertIsNotNone(self.user_manager.userbase, "Userbase failed to automatically initialise.")
 
         # Clear userbase to test reloading
         self.user_manager.userbase = None
         self.assertIsNone(self.user_manager.userbase, "Userbase was not cleared.")
 
+        # Reload userbase
         self.user_manager.load_userbase(self.test_users_file)
         self.assertIsNotNone(self.user_manager.userbase, "Userbase failed to be reloaded.")
 
@@ -48,12 +65,88 @@ class TestUserManager(TestCase):
 
         user = self.user_manager.sign_in(realuser, realpass)
         self.assertEqual(user["username"], realuser, "Username does not match given one.")
-        self.assertEqual(user["password"], realpass, "Password does not match given one.")
         self.assertEqual(user["balance"], 100, "Balance does not match known value.")
 
     def test_save_to_csv(self):
         _logger.info("Testing save_to_csv()")
-        self.fail()
+
+        self.assertFalse(self.user_manager.save_to_csv(csv_path=None))
+        self.assertFalse(self.user_manager.save_to_csv("//Invalid filepath"))
+
+        self.assertIsNotNone(self.test_users_file)
+
+        self.assertIsNotNone(self.user_manager.userbase)
+
+        usercount = len(self.user_manager.userbase.index)
+        self.assertTrue(self.user_manager.register_user("newuser", "newpass", 50.0))
+        self.assertEqual(len(self.user_manager.userbase.index), usercount + 1)
+
+        self.assertTrue(self.user_manager.save_to_csv(self.test_users_file))
+
+        self.user_manager.userbase = None
+        self.assertFalse(self.user_manager.save_to_csv(csv_path=self.test_users_file))
+
+    def test_sign_out(self):
+        _logger.info("Testing sign_out()")
+
+        self.assertIsNone(self.user_manager.current_user)
+
+        self.user_manager.current_user = self.user_manager.guest_account()
+
+        current_user = self.user_manager.current_user
+
+        self.assertIsNotNone(current_user)
+        self.assertEqual(current_user["username"], "Guest", "Account username is incorrect.")
+        self.assertEqual(current_user["password"], None, "Account password is incorrect.")
+        self.assertEqual(current_user["balance"], 0, "Account balance is incorrect.")
+
+        self.user_manager.sign_out()
+
+        self.assertIsNone(self.user_manager.current_user)
+
+    def test_registration(self):
+
+        # Register valid user should succeed
+        usercount = len(self.user_manager.userbase.index)
+        self.assertEqual("Registration successful.", self.user_manager.register_user("newuser", "newpass", starting_balance=50.0))
+        self.assertEqual(usercount + 1, len(self.user_manager.userbase.index))
+
+        # Register user with existing username should fail
+        usercount = len(self.user_manager.userbase.index)
+        self.assertEqual("Username already exists.", self.user_manager.register_user("newuser", "newpass", starting_balance=50.0))
+        self.assertEqual(usercount, len(self.user_manager.userbase.index))
+
+        # Register user with no balance should default to 0
+        usercount = len(self.user_manager.userbase.index)
+        self.assertEqual("Registration successful.", self.user_manager.register_user("michellejones", "pass123"))
+        self.assertEqual(usercount + 1, len(self.user_manager.userbase.index))
+        self.assertEqual(self.user_manager.userbase[["michellejones"]]["balance"], 0)
+
+        # Register user with no userbase set should fail
+
+        #
+        # def register_user(self, username: str, password: str, starting_balance=0):
+        #     if self.userbase is None:
+        #         _logger.error("Userbase not loaded. Call `load_userbase()` first.")
+        #         return "User registration unavailable. User database has not been initialised."
+        #     if password is None:
+        #         _logger.error("Password is invalid.")
+        #         return "Password is invalid."
+        #     try:
+        #         if username in self.userbase.index:
+        #             return "Username already exists."
+        #         else:
+        #             user_details = {
+        #                 "password": password,
+        #                 "balance": starting_balance
+        #             }
+        #             self.userbase[username] = user_details
+        #             print(self.userbase)
+        #             return "Registration successful."
+        #
+        #     except Exception as ex:
+        #         _logger.error(f"An exception occurred: {ex}")
+        #         return "An exception occurred:"
 
     def test_guest_account(self):
         _logger.info("Testing guest_account()")
--- users.py
+++ users.py
@@ -6,40 +6,164 @@ _logger = logging.getLogger(__name__)
 from pandas import read_csv
 
 class UserManager:
-    def __init__(self, csv_path="data/users.csv", *args, **kwargs):
-        # Load data from csv by default
-        self.load_userbase(csv_path=csv_path)
+    """
+    A simple manager for user accounts.
+    WARNING: This implementation is insecure and should not be used in production environments.
+    """
+
+    def __init__(self, csv_path="data/users.csv"):
+        """
+        Initalise a UserManager instance.
+        :param csv_path: The path to use for storing the user csv file.
+        """
+        # Set default values
+        self.userbase = None
+        self.current_user = None
+
+        if csv_path is not None:
+            self.load_userbase(csv_path=csv_path)
 
     def load_userbase(self, csv_path="data/users.csv"):
-        _logger.debug("Loading userbase from local file")
-        try:
-            self.userbase = read_csv(csv_path, index_col=0)
-        except FileNotFoundError as ex:
-            _logger.error(f"Local storage file '{csv_path}' not found: {ex}")
+        """
+        Load a local csv file to get user information.
+        This implementation uses unsecured csv files containing plaintext passwords.
+        :param csv_path: The path of the user csv file to load.
+        :return: None.
+        """
+        # Load from given file
+        if csv_path is not None:
+            _logger.debug("Loading userbase from local file.")
+            try:
+                # Read csv file using first column (username) as the index
+                userbase = read_csv(csv_path, index_col=0)
+                # Check userbase was successfully loaded
+                if userbase is None:
+                    raise Exception(f"Userbase {csv_path} not successfully read.")
+                # Set userbase as global reference
+                self.userbase = userbase
+            except FileNotFoundError as ex:
+                _logger.error(f"Local storage file '{csv_path}' not found: {ex}")
+            except Exception as ex:
+                _logger.error(f"An exception occurred: {ex}")
+        else:
+            _logger.error(f"No local storage file specified.")
 
     def sign_in(self, username, password):
+        """
+        Sign in a user. This will set the current_user attribute to a dictionary containing the user's details.
+        :param username: The username of the user being signed in.
+        :param password: The password entered to sign in.
+        :return: A dictionary of the current user's details if successfully verified. Otherwise, None.
+        """
+        # Check userbase exists
         if self.userbase is None:
             _logger.error("Userbase not loaded. Call `load_userbase()` first.")
             return None
         try:
+            # Select user row by (unique) username
             user = self.userbase.loc[[username]]
+
+            # Check if user is found
+            if user is None:
+                raise KeyError()
+
+            # Format user details as a dict
             user.reset_index(inplace=True) # Move username from index to new column
-            user = user.to_dict('records')[0] # Format user as a dict
+            records = user.to_dict('records')
+            if isinstance(records, list): # Confirm single element list containing a dict is returned
+                if len(records) != 1:
+                    raise Exception(f"{len(records)} records found matching username '{username}'.")
+                # Get first user record
+                record = records[0]
 
-            # Confirm user found and password matches
-            if user is None or user["password"] != password:
-                raise KeyError("Invalid username or password")
+                if not isinstance(record, dict): # Confirm user record is a dict
+                    raise Exception(f"{record} is not a dict.")
+            else:
+                raise Exception(f"{records} is not a list of users.")
 
-            return user
+            # Confirm a user has been found
+            if record is not None:
+                # Confirm password matches
+                if record["password"] != password:
+                    raise KeyError()
+
+                # Sign out current user if any
+                if self.current_user is not None:
+                    self.sign_out()
+
+                # Set new user as current
+                del record["password"] # Remove password before returning dict
+                self.current_user = record
+                return self.current_user
+
         except KeyError as ex:
-            _logger.info("Invalid username or password")
+            _logger.info("Invalid username or password.")
             return None
+        except Exception as ex:
+            _logger.error(f"An exception occurred: {ex}")
+
+    def sign_out(self):
+        """
+        Sign out the current user.
+        :return: None.
+        """
+        if self.current_user is not None:
+            self.current_user = None
+        else:
+            _logger.warning("No current user to sign out.")
 
     def save_to_csv(self, csv_path="data/users.csv"):
-        pass
-        # self.prices_df.to_csv(csv_path)
+        """
+        Save out userbase to a csv file.
+        :param csv_path: The path of the new csv file.
+        :return: True if successful. False otherwise.
+        """
+        try:
+            if csv_path is None:
+                raise Exception("No local storage file specified.")
+
+            elif self.userbase is None:
+                raise Exception("No user data to save.")
+
+            # Save to given file
+            self.userbase.to_csv(csv_path)
+            return True
+
+        except FileNotFoundError as ex:
+            _logger.error(f"Local storage file '{csv_path}' not found: {ex}")
+        except Exception as ex:
+            _logger.error(f"An exception occurred: {ex}")
+
+        # Save failed
+        return False
+
+    def register_user(self, username : str, password : str, starting_balance=0):
+        if self.userbase is None:
+            _logger.error("Userbase not loaded. Call `load_userbase()` first.")
+            return "User registration unavailable. User database has not been initialised."
+        if password is None:
+            _logger.error("Password is invalid.")
+            return "Password is invalid."
+        try:
+            if username in self.userbase.index:
+                return "Username already exists."
+            else:
+                user_details = {
+                    "password": password,
+                    "balance": starting_balance
+                }
+                self.userbase.loc[username] = user_details
+                return "Registration successful."
+
+        except Exception as ex:
+            _logger.error(f"An exception occurred: {ex}")
+            return "An exception occurred."
 
     def guest_account(self):
+        """
+        Return a blank guest account with default credentials.
+        :return: A user account dict containing credentials for a guest user.
+        """
         return {
             "username": "Guest",
             "password": None,
