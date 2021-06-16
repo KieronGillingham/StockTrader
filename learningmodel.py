b9767d2e758d6a54f62735eb49ffb207b70ae794
Kieron Gillingham <kierongillingham@gmail.com>
2021-06-16 09:50:15 +0100

    persisting model between predictions and date decomposition

--- learningmodel.py
+++ learningmodel.py
@@ -14,6 +14,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 
 class LearningModel():
 
@@ -26,10 +27,11 @@ class LearningModel():
 
     def __init__(self, data=None, *args, **kwargs):
         self.data = data
+        self.model = None
 
     def predict(self, stock, prediction_period='NEXTMONTH'):
 
-        _logger.debug(self.data)
+        _logger.debug(f"Loaded data shape: {self.data.shape}")
 
         if not isinstance(stock, str):
             raise TypeError("Stock must be a string of a stock symbol: I.E. GOOG")
@@ -45,14 +47,21 @@ class LearningModel():
         _logger.debug(f"Latest date: {latest_date_stamp} / {date.fromtimestamp(latest_date_stamp)}")
         _logger.debug(f"Prediction date ({prediction_period}): {prediction_date_stamp} / {date.fromtimestamp(prediction_date_stamp)}")
 
-
+        print(self.deconstruct_date([latest_date_stamp, prediction_date_stamp]))
 
         #return self.linear_model_prediction(stock, prediction_date_stamp)
         #return self.mlp_model_prediction(stock, prediction_date_stamp)
 
         prediction_date_stamp = latest_date_stamp
         latest_date_stamp = prediction_date_stamp - self.PERIODS[prediction_period]
-        return self.mlp_model_prediction(stock, prediction_date_stamp, latest_date_stamp)
+        # return self.naive_rolling_mlp_model_prediction(stock, prediction_date_stamp, latest_date_stamp)
+        if self.model is None:
+            self.date_decomposed_mlp_model_training()
+        dates = [(latest_date_stamp + (i * 604800)) for i in range(0, 5)]
+        predictions = self.model.predict(self.deconstruct_date(dates))
+        print(predictions)
+
+        return pd.DataFrame(data=predictions, index=dates, columns=self.data.columns[:30])[f"{stock}_close"]
 
     def linear_model_prediction(self, stock, prediction_date_stamp):
         latest_date_stamp = self.data.index.max()
@@ -75,7 +84,7 @@ class LearningModel():
 
         return pred_df
 
-    def mlp_model_prediction(self, stock, prediction_date_stamp, latest_date_stamp=None):
+    def naive_rolling_mlp_model_prediction(self, stock, prediction_date_stamp, latest_date_stamp=None):
 
         print(latest_date_stamp)
         print(prediction_date_stamp)
@@ -131,6 +140,9 @@ class LearningModel():
             if prediction_date[0] == normed_latest_date_stamp:
                 continue
             regressor.fit(x, y)
+
+            self.model = regressor
+
             print(regressor.best_params_)
 
             # Single date prediction
@@ -159,9 +171,145 @@ class LearningModel():
 
         return pred_df
 
+    def date_decomposed_mlp_model_training(self, data=None):
+
+        if data is None:
+            data = self.data
+
+        if data is None:
+            _logger.error("Data is not set for training.")
+            return
+
+        data.sort_index(inplace=True)
+        earliest_date_stamp = data.index.min()
+        latest_date_stamp = data.index.max()
+
+        _logger.debug(f"Preparing to train model between {date.fromtimestamp(earliest_date_stamp)} and "
+                      f"{date.fromtimestamp(latest_date_stamp)}.")
+
+        timecolumns = ['year', 'month', 'day', 'weekday', 'week', 'dayofyear']
+
+        x = data[timecolumns]
+        _logger.debug(x)
+        # x = x.reshape(-1, 1)
+        y = data.drop(timecolumns, 1)
+        _logger.debug(y)
+
+
+        #
+        # # Scaling
+        # _logger.debug("Scaling (y)")
+        # scaler = StandardScaler()
+        # scaler.fit(y)
+        # y = scaler.transform(y)
+        # _logger.debug(y)
+        #
+        params = [
+            {
+                "random_state": [98629],
+                "solver": ["adam"],
+                "hidden_layer_sizes": [(10, 5), (10, 5, 10), (10, 5, 2), (40, 60, 40)],
+                "max_iter": [500],
+                "verbose": [True]
+            }
+        ]
+
+        regressor = GridSearchCV(MLPRegressor(), params)
+        regressor.fit(x, y)
+        self.model = regressor
+
+        #
+        #
+        # for prediction_date in prediction_dates:
+        #     if prediction_date[0] == normed_latest_date_stamp:
+        #         continue
+        #
+        #
+        #
+        #
+        #     print(regressor.best_params_)
+        #
+        #     # Single date prediction
+        #     # prediction = model.predict([[prediction_date_stamp]])
+        #     # _logger.debug(f"{self.data.loc[latest_date_stamp].values} -> {prediction[0]}")
+        #     # pred_df = pd.DataFrame([self.data.loc[latest_date_stamp].values, prediction[0]],
+        #     #                       columns=self.data.columns, index=[latest_date_stamp, prediction_date_stamp])
+        #
+        #     prediction = regressor.predict([prediction_date])
+        #     predictions.append(prediction[0])
+        #     #x = np.append(x, prediction_date, axis=0)
+        #     x = np.concatenate([x, [prediction_date]], axis=0)
+        #     y = np.concatenate([y, [prediction[0]]], axis=0)
+        #
+        # predictions = scaler.inverse_transform(predictions)
+        # _logger.debug(f"Test Data: {test_data}")
+        # _logger.debug(f"Predictions: {predictions}")
+        # unscaled_prediction_dates = [(i[0] + earliest_date_stamp) for i in prediction_dates]
+        # pred_df = pd.DataFrame(predictions, columns=self.data.columns, index=unscaled_prediction_dates)
+        #
+        #
+        #
+        # # Filter by stock
+        # pred_df = pred_df[f"{stock}_close"]
+        # _logger.debug(pred_df)
+        # # Using mean for naïve forecasting
+        # _logger.debug(f"Historical data mean: {historical_data[f'{stock}_close'].mean()}")
+        #
+        # return pred_df
+
+    def deconstruct_date(self, datestamps):
+
+        dates = [date.fromtimestamp(datestamp) for datestamp in datestamps]
+
+        deconstructed_dates = [
+            [
+                dateobj.year,
+                dateobj.month,
+                dateobj.day,
+                dateobj.weekday(),
+                dateobj.isocalendar()[1], # Week number
+                int(dateobj.strftime('%j')) # Day of year
+            ]
+            for dateobj in dates
+        ]
+        print(deconstructed_dates)
+
+        columns = ['year', 'month', 'day', 'weekday', 'week', 'dayofyear']
+        dates = pd.DataFrame(data=deconstructed_dates, index=datestamps, columns=columns)
+        return dates
+
+        # print(pd.to_datetime(input_dates, format="%d/%m/%Y"))
+        # print(dates)
+
+        # dates['Date'] = pd.to_datetime(input_dates)
+        # dates['Date'] = dates['Date'].dt.strftime('%d.%m.%Y')
+        # dates['year'] = pd.DatetimeIndex(dates['Date']).year
+        # dates['month'] = pd.DatetimeIndex(dates['Date']).month
+        # dates['day'] = pd.DatetimeIndex(dates['Date']).day
+        # dates['dayofyear'] = pd.DatetimeIndex(dates['Date']).dayofyear
+        # dates['weekofyear'] = pd.DatetimeIndex(dates['Date']).weekofyear
+        # dates['weekday'] = pd.DatetimeIndex(dates['Date']).weekday
+        # dates['quarter'] = pd.DatetimeIndex(dates['Date']).quarter
+        # dates['is_month_start'] = pd.DatetimeIndex(dates['Date']).is_month_start
+        # dates['is_month_end'] = pd.DatetimeIndex(dates['Date']).is_month_end
+        #
+        # print(dates)
+
+        #input_date = date.fromtimestamp(input_date)
+        #return [
+        #    input_date.year,
+        #    input_date.month,
+        #    input_date.day,
+        #    input_date.weekday()
+        #]
 
     def set_data(self, data):
         if isinstance(data, pd.DataFrame):
+            if not {"year", "month", "day", "weekday", "week", "dayofyear"}.issubset(data.columns):
+                print(data.shape)
+                data = data.join(self.deconstruct_date(data.index))
+                print(data.shape)
+                print(data)
             self.data = data
             return True
         else:
@@ -174,5 +322,22 @@ class LearningModel():
         elif not isinstance(self.data, pd.DataFrame):
             _logger.error("Dataset is not a dataframe. Please use `set_data()` to recreate the dataframe.")
             return False
-        else:
-            return True
+        elif self.data.isnull().any().any():
+            _logger.info(f"{self.data.isnull().sum()} missing values in dataset. Imputing substitute values.")
+            imputer = SimpleImputer()
+            imputer.fit(self.data.index, self.data.values)
+            imputer.transform(self.data.index)
+        return True
+
+    def calculate_return(self, investments : list):
+        if self.model is None:
+            # Create model first
+            _logger.error("No model exists.")
+            return
+
+        for investment in investments:
+            if not isinstance(investment, tuple):
+                _logger.error(f"Investment format incorrect - Not a tuple: {investment}")
+                return
+            else:
+                return
